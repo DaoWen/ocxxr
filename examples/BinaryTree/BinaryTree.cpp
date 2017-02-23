54555bc426a6cb71705d93c106680e1f4c31019e
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2017-02-23 17:17:16 -0600

    Add BinaryTree example

---
+++ examples/BinaryTree/BinaryTree.cpp
@@ -0,0 +1,146 @@
+/*
+ * BinaryTree.cpp
+ *
+ * A simple tree-based mapping data structure.
+ */
+
+#include <ocxxr-main.hpp>
+
+#include <cstdlib>
+
+constexpr bool kVerboseMessages = false;
+
+template <typename T>
+using Ptr = ocxxr::RelPtr<T>;
+
+template <typename K, typename V>
+class BinaryTree {
+ public:
+    static constexpr size_t kTreeSize =
+            (1 << 20);  // Allocate enough space for 1M entries
+
+    static ocxxr::Arena<BinaryTree> Create() {
+        constexpr size_t bytes = sizeof(BinaryTree) + sizeof(Node) * kTreeSize;
+        auto arena = ocxxr::Arena<BinaryTree>::Create(bytes);
+        CreateIn(arena.Untyped());
+        return arena;
+    }
+
+    static BinaryTree *CreateIn(ocxxr::Arena<void> arena) {
+        return arena.New<BinaryTree>(arena);
+    }
+
+    BinaryTree(ocxxr::Arena<void> arena) : arena_(arena), root_(nullptr) {}
+
+    bool Put(const K &key, const V &value) {
+        if (kVerboseMessages) {
+            PRINTF("Starting put op...\n");
+        }
+        return Update(key, value, root_);
+    }
+
+    bool Get(const K &key, V &result) {
+        if (kVerboseMessages) {
+            PRINTF("Starting get op...\n");
+        }
+        return Find(key, root_, result);
+    }
+
+    // TODO - add remove op, and use a freelist internally for nodes
+
+ private:
+    struct Node {
+        const K key;
+        V value;
+        Ptr<Node> left, right;
+        Node(const K &k, const V &v)
+                : key(k), value(v), left(nullptr), right(nullptr) {}
+    };
+
+    ocxxr::Arena<void> arena_;
+    Ptr<Node> root_;
+
+    bool Update(const K &key, const V &value, Ptr<Node> &node) {
+        if (!node) {
+            // Base case 1: not found
+            node = arena_.New<Node>(key, value);
+            return false;
+        } else if (node->key == key) {
+            // Base case 2: found
+            node->value = value;
+            return true;
+        } else {
+            // Recursive case
+            Ptr<Node> &next = (node->key < key) ? node->left : node->right;
+            return Update(key, value, next);
+        }
+    };
+
+    bool Find(const K &key, Ptr<Node> node, V &output) {
+        if (!node) {
+            // Base case 1: not found
+            return false;
+        } else if (node->key == key) {
+            // Base case 2: found
+            output = node->value;
+            return true;
+        } else {
+            // Recursive case
+            Ptr<Node> &next = (node->key < key) ? node->left : node->right;
+            return Find(key, next, output);
+        }
+    };
+};
+
+void ocxxr::Main(ocxxr::Datablock<ocxxr::MainTaskArgs> args) {
+    u32 n;
+    if (args->argc() != 2) {
+        n = 100000;
+        PRINTF("Usage: BinaryTree <num>, defaulting to %" PRIu32 "\n", n);
+    } else {
+        n = atoi(args->argv(1));
+    }
+
+    auto tree = BinaryTree<u64, char>::Create();
+    auto hash = std::hash<u64>{};
+
+    //
+    // Puts
+    //
+
+    constexpr u64 kPutCount = 10;
+
+    if (kVerboseMessages) {
+        PRINTF("Starting puts\n");
+    }
+
+    for (u64 i = 0; i < kPutCount; i++) {
+        tree->Put(hash(i), static_cast<char>('a' + i));
+    }
+
+    if (kVerboseMessages) {
+        PRINTF("Done with puts\n");
+    }
+
+    //
+    // Gets
+    //
+
+    if (kVerboseMessages) {
+        PRINTF("Starting gets\n");
+    }
+
+    char result[] = {'X', '\0'};
+    tree->Get(hash(6), result[0]);
+
+    if (kVerboseMessages) {
+        PRINTF("Done with gets\n");
+    }
+
+    //
+    // Results
+    //
+
+    PRINTF("Result = %s\n", result);
+    ocxxr::Shutdown();
+}
