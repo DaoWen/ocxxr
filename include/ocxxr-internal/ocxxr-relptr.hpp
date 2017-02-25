44e85b734ffb2f8aca791a497054c7f1f9854e72
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2017-02-24 18:10:38 -0600

    Reorder BasedPtr/RelPtr definitions
    (This is part of refactoring BasedPtr into BasedPtr/BasedDbPtr)

--- include/ocxxr-internal/ocxxr-relptr.hpp
+++ include/ocxxr-internal/ocxxr-relptr.hpp
@@ -4,78 +4,7 @@
 namespace ocxxr {
 
 template <typename T>
-class BasedPtr;
-
-/**
- * This is our "relative pointer" class.
- * You should be able to use it pretty much just like a normal pointer.
- * However, you still need to be careful to only point to memory within
- * the same datablock. Nothing keeps you from creating a "relative pointer"
- * into another datablock, or even into the stack.
- */
-template <typename T>
-class RelPtr {
- public:
-    // offset of 1 is impossible since this is larger than 1 byte
-    constexpr RelPtr() : offset_(1) {}
-
-    RelPtr(const RelPtr &other) { set(other); }
-
-    RelPtr(const T *other) { set(other); }
-
-    // TODO - ensure that default assignment operator still works correctly
-    RelPtr<T> &operator=(const RelPtr &other) {
-        set(other);
-        return *this;
-    }
-
-    RelPtr<T> &operator=(const T *other) {
-        set(other);
-        return *this;
-    }
-
-    T &operator*() const { return *get(); }
-
-    T *operator->() const { return get(); }
-
-    T &operator[](const int index) const { return get()[index]; }
-
-    operator T *() const { return get(); }
-
-    operator BasedPtr<T>() const { return get(); }
-
-    bool operator!() const { return offset_ == 0; }
-
-    bool operator==(const RelPtr &other) const { return get() == other.get(); }
-
-    // TODO - implement math operators, like increment and decrement
-
- private:
-    ptrdiff_t offset_;
-
-    ptrdiff_t base_ptr() const { return reinterpret_cast<ptrdiff_t>(this); }
-
-    void set(const RelPtr &other) { set(other.get()); }
-
-    void set(const T *other) {
-        if (other == nullptr) {
-            offset_ = 0;
-        } else {
-            ptrdiff_t ptr = reinterpret_cast<ptrdiff_t>(other);
-            offset_ = internal::CombineBaseOffset(-base_ptr(), ptr);
-        }
-    }
-
-    T *get() const {
-        assert(offset_ != 1);
-        if (offset_ == 0) {
-            return nullptr;
-        } else {
-            ptrdiff_t target = internal::CombineBaseOffset(base_ptr(), offset_);
-            return reinterpret_cast<T *>(target);
-        }
-    }
-};
+class RelPtr;
 
 /**
  * This is our "based pointer" class.
@@ -183,6 +112,77 @@ class BasedPtr {
             ptrdiff_t base = internal::AddressForGuid(target_guid_);
 #endif  // OCXXR_USE_NATIVE_POINTERS
             ptrdiff_t target = internal::CombineBaseOffset(base, offset_);
+            return reinterpret_cast<T *>(target);
+        }
+    }
+};
+
+/**
+ * This is our "relative pointer" class.
+ * You should be able to use it pretty much just like a normal pointer.
+ * However, you still need to be careful to only point to memory within
+ * the same datablock. Nothing keeps you from creating a "relative pointer"
+ * into another datablock, or even into the stack.
+ */
+template <typename T>
+class RelPtr {
+ public:
+    // offset of 1 is impossible since this is larger than 1 byte
+    constexpr RelPtr() : offset_(1) {}
+
+    RelPtr(const RelPtr &other) { set(other); }
+
+    RelPtr(const T *other) { set(other); }
+
+    // TODO - ensure that default assignment operator still works correctly
+    RelPtr<T> &operator=(const RelPtr &other) {
+        set(other);
+        return *this;
+    }
+
+    RelPtr<T> &operator=(const T *other) {
+        set(other);
+        return *this;
+    }
+
+    T &operator*() const { return *get(); }
+
+    T *operator->() const { return get(); }
+
+    T &operator[](const int index) const { return get()[index]; }
+
+    operator T *() const { return get(); }
+
+    operator BasedPtr<T>() const { return get(); }
+
+    bool operator!() const { return offset_ == 0; }
+
+    bool operator==(const RelPtr &other) const { return get() == other.get(); }
+
+    // TODO - implement math operators, like increment and decrement
+
+ private:
+    ptrdiff_t offset_;
+
+    ptrdiff_t base_ptr() const { return reinterpret_cast<ptrdiff_t>(this); }
+
+    void set(const RelPtr &other) { set(other.get()); }
+
+    void set(const T *other) {
+        if (other == nullptr) {
+            offset_ = 0;
+        } else {
+            ptrdiff_t ptr = reinterpret_cast<ptrdiff_t>(other);
+            offset_ = internal::CombineBaseOffset(-base_ptr(), ptr);
+        }
+    }
+
+    T *get() const {
+        assert(offset_ != 1);
+        if (offset_ == 0) {
+            return nullptr;
+        } else {
+            ptrdiff_t target = internal::CombineBaseOffset(base_ptr(), offset_);
             return reinterpret_cast<T *>(target);
         }
     }
