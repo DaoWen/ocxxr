8917709deb041f2b43223a01c6ea9bf351a70a19
Lechen Yu <ylcmen@gmail.com>
2017-02-26 23:42:05 -0600

    add errorbar to the graph

--- examples/draw.py
+++ examples/draw.py
@@ -14,33 +14,41 @@ from toolz.itertoolz import groupby
 from operator import itemgetter
 from csv import DictReader
 
+base_line_index = 1
 input = 'result.dat'
-
 f = open(input, 'r')
 legends = f.readline().strip().split(' ');
+raw = {}
 datas = {}
-datas_mean = {}
-
+datas['mean'] = {}
+datas['error'] = {}
 for legend in legends:
-    datas[legend] = {}
+    raw[legend] = {}
 
 benchmarks = []
 for line in f:
     line = line.strip();
     elems = line.split(" ")
     if (elems[0] == '#'):
-        if (not elems[1] in datas[elems[2]]):
-            datas[elems[2]][elems[1]] = []
+        if (not elems[1] in raw[elems[2]]):
+            raw[elems[2]][elems[1]] = []
         if (not elems[1] in benchmarks):
             benchmarks.append(elems[1])
-        currentArray = datas[elems[2]][elems[1]]
+        currentArray = raw[elems[2]][elems[1]]
     else:
         currentArray.append(float(line));
 
-for key, value in datas.items():
-    datas_mean[key] = [np.array(value[benchmark]).mean() for benchmark in benchmarks]
+for key, value in raw.items():
+    datas['mean'][key] = np.array([np.array(value[benchmark]).mean() for benchmark in benchmarks])
 
-     
+base_line = [x for x in datas['mean'][legends[base_line_index]]]
+for key, value in datas['mean'].items():
+    datas['mean'][key] = np.array(value) / np.array(base_line)
+
+for key, value in raw.items():
+    standard_error = np.array([np.std(np.array(value[benchmark]) / base_line[i]) / np.sqrt(len(value[benchmark])) for i, benchmark in enumerate(benchmarks)])
+    datas['error'][key] = [standard_error, standard_error]
+
 fig, ax = plt.subplots(1)
 fig.set_size_inches(6.0,3.5)
 filename = os.path.splitext(os.path.basename(__file__))[0]
@@ -48,17 +56,18 @@ filename = os.path.splitext(os.path.basename(__file__))[0]
 ind = np.arange(len(benchmarks))
 W = 0.25
 colors = palettable.tableau.Gray_5.mpl_colors
+errkw=dict(ecolor='black', lw=2, capthick=1)
 
 
 rects = []
 
 for i, legend in enumerate(legends):
-    rects.append(ax.bar(ind + i * W, datas_mean[legend], W, color=colors[i]))
+    rects.append(ax.bar(ind + i * W, datas['mean'][legend], W, color=colors[i], yerr=datas['error'][legend], error_kw=errkw))
 
 ax.set_xticks(ind + W / 2)
 ax.set_xticklabels(benchmarks)
 plt.xlabel("Benchmark")
-plt.ylabel("Execution Time (seconds)")
+plt.ylabel("Slowdown")
 plt.legend(rects, legends, loc='upper right').draw_frame(False)
 plt.savefig(filename + '.eps', bbox_inches='tight', format='eps')
 
