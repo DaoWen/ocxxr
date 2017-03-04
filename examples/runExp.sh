6910b1e74320828a39b2d5c8ba232ce6d901fad7
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2017-03-04 13:03:18 -0600

    Refactor experiments run script

--- examples/runExp.sh
+++ examples/runExp.sh
@@ -1,96 +1,75 @@
 #!/bin/bash
 
-thread_num=1
-iteration=10
-outputFile=$(pwd)/result.dat
+set -e
 
 # defaults
 [ -z $XSTG_ROOT ] && export XSTG_ROOT=$(cd ../.. && pwd)
 [ -z $OCR_INSTALL_ROOT ] && export OCR_INSTALL_ROOT=$XSTG_ROOT/ocr/ocr/install
 
+run_benchmarks() {
+    # arguments
+    local label=$1
+    local flags=$2
+    local awk_cmd=$3
+    local iterations=${4}
+    # constants
+    local thread_num=1
+    # go!
+    echo "$label" >> $OUTPUT_FILE
+    for dir in $BENCHMARKS; do
+        if [ -d "$dir" ] && [ $dir != "makefiles" ]; then
+            printf "\n\n> Running test %s\n\n" $dir
+            echo "# $dir $label" >> $OUTPUT_FILE
+            pushd $dir > /dev/null
+            make -f Makefile.x86 clean
+            for ((index = 0; index < iterations; index++)); do
+                CONFIG_NUM_THREADS=$thread_num NO_DEBUG=yes OCR_TYPE=x86 CFLAGS="-DNDEBUG=1 $flags" \
+                    make -f Makefile.x86 run | awk "$awk_cmd" >> $OUTPUT_FILE
+            done
+            popd > /dev/null
+        fi
+    done
+}
+
+export EXPERIMENT_TYPE="$1"
+
 # Which benchmarks to run?
 if [ -z "$2" ]; then
-    BENCHMARKS="BinaryTree Hashtable Tempest Lulesh"
+    export BENCHMARKS="BinaryTree Hashtable Tempest Lulesh"
 else
-    BENCHMARKS="$2"
-    for d in $BENCHMARKS; do
-        if ! [ -d $d ]; then
-            echo "Experiment directory not found: $d"
-            exit 1
-        fi
-    done
+    export BENCHMARKS="$2"
 fi
-
-if [ -z $1 ]; then
-    echo "Please specify experiment name: [time|op-count]"
-elif [ $1 = "time" ]; then
-    if [ -e "$outputFile" ]; then
-        rm $outputFile
+for d in $BENCHMARKS; do
+    if ! [ -d $d ]; then
+        echo "Experiment directory not found: $d"
+        exit 1
     fi
-    touch $outputFile
+done
 
-    echo "offset native" >> $outputFile
+export OUTPUT_FILE=$PWD/result.dat
+
+if [ "$EXPERIMENT_TYPE" = "time" ]; then
+    rm -f $OUTPUT_FILE
+    echo "offset native" >> $OUTPUT_FILE
+    BASE_FLAGS="-DMEASURE_TIME=1"
+    ITERS=10
+    AWK_CMD='/elapsed time:/ { print $3 }'
     # original version
-    printf "\n\n--------------------Offset Based Relptr--------------------"
-    for dir in $BENCHMARKS; do
-        if [ -d "$dir" ] && [ $dir != "makefiles" ]; then
-            printf "\n\n> Running test %s\n\n" $dir
-            echo \# $dir offset >> $outputFile
-            pushd $dir > /dev/null
-            index=0
-            make -f Makefile.x86 clean
-            while [ $index -lt $iteration ]
-            do
-                CONFIG_NUM_THREADS=$thread_num NO_DEBUG=yes OCR_TYPE=x86 CFLAGS="-DMEASURE_TIME=1 -DNDEBUG=1" make -f Makefile.x86 run | awk '/elapsed time:/ { print $3 }' >> $outputFile
-                index=$((index+1))
-            done
-            popd > /dev/null
-        fi
-    done
-
+    printf "\n\n--------------------Position Independent Pointers--------------------\n"
+    run_benchmarks "position independent" "$BASE_FLAGS" "$AWK_CMD" $ITERS
     # native pointer version
-    printf "\n\n--------------------Native Pointer Based Relptr--------------------"
-    for dir in $BENCHMARKS; do
-        if [ -d "$dir" ] && [ $dir != "makefiles" ]; then
-            printf "\n\n> Running test %s\n\n" $dir
-            echo \# $dir native >> $outputFile
-            pushd $dir > /dev/null
-            index=0
-            make -f Makefile.x86 clean
-            while [ $index -lt $iteration ]
-            do
-                CONFIG_NUM_THREADS=$thread_num NO_DEBUG=yes OCR_TYPE=x86 CFLAGS="-DOCXXR_USE_NATIVE_POINTERS=1 -DMEASURE_TIME=1 -DNDEBUG=1" make -f Makefile.x86 run | awk '/elapsed time:/ { print $3 }' >> $outputFile
-                index=$((index+1))
-            done
-            popd > /dev/null
-        fi
-    done
+    printf "\n\n--------------------Native Pointers--------------------\n"
+    run_benchmarks "native" "-DOCXXR_USE_NATIVE_POINTERS=1 $BASE_FLAGS" "$AWK_CMD" $ITERS
+    # plot
     python draw.py
-elif [ $1 = "op-count" ]; then
-    if [ -e "$outputFile" ]; then
-        rm $outputFile
-    fi
-    touch $outputFile
-    iteration=1
-
+elif [ "$EXPERIMENT_TYPE" = "op-count" ]; then
+    rm -f $OUTPUT_FILE
+    BASE_FLAGS="-DINSTRUMENT_POINTER_OP=1"
+    ITERS=1
+    AWK_CMD='/^rp|^bp/'
     # instrumented version
-    printf "\n\n--------------------Count Operation--------------------"
-    for dir in $BENCHMARKS; do
-        if [ -d "$dir" ] && [ $dir != "makefiles" ]; then
-            printf "\n\n> Running test %s\n\n" $dir
-            echo \# $dir>> $outputFile
-            pushd $dir > /dev/null
-            index=0
-            make -f Makefile.x86 clean
-            while [ $index -lt $iteration ]
-            do
-                CONFIG_NUM_THREADS=$thread_num NO_DEBUG=yes OCR_TYPE=x86 CFLAGS="-DINSTRUMENT_POINTER_OP=1 -DNDEBUG=1" make -f Makefile.x86 run | awk '/^rp|^bp/'>> $outputFile
-                index=$((index+1))
-            done
-            popd > /dev/null
-        fi
-    done
-
+    printf "\n\n--------------------Count Operations--------------------\n"
+    run_benchmarks "op count" "$BASE_FLAGS" "$AWK_CMD" $ITERS
 else
     echo "Please specify experiment name: [time|op-count]"
 fi
