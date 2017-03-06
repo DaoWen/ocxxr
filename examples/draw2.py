8cd0e2ff06f853171ae511b10f96e1740ed8bb06
Lechen Yu <ylcmen@gmail.com>
2017-03-05 18:06:29 -0600

    make operation count graph looks better

--- examples/draw2.py
+++ examples/draw2.py
@@ -39,25 +39,23 @@ for line in f:
 raw['bp_total'] = np.array(raw['bp_get_count']) + np.array(raw['bp_set_count'])
 raw['rp_total'] = np.array(raw['rp_get_count']) + np.array(raw['rp_set_count'])
 
-#yindex = np.arange(6)
 fig, ax = plt.subplots(1)
 fig.set_size_inches(8.0,3.5)
-#plt.semilogy(yindex, np.exp(yindex))
 #filename = os.path.splitext(os.path.basename(__file__))[0]
 
 W = 0.2
-colors = palettable.tableau.Gray_5.mpl_colors
-
+#colors = palettable.tableau.Gray_5.mpl_colors
+colors = palettable.cmocean.sequential.Gray_20.mpl_colors
 
 rects = []
 index = np.arange(len(benchmarks)) * 1.5
 for i, key in enumerate(keys):
-    rects.append(ax.bar(index + i * W, raw[key], W, color=colors[0], log=True))
+    rects.append(ax.bar(index + i * W, raw[key], W, color=colors[(i + 1) * 3], bottom=0.001, log=True))
 
 ax.set_xticks(index + W)
 ax.set_xticklabels(benchmarks)
-plt.xlabel("Pointer Type")
-plt.ylabel("Slowdown")
+plt.xlabel("Benchmark")
+plt.ylabel("Frequency(op/ms)")
 plt.legend(rects, legends, loc='lower left', bbox_to_anchor=(0, -0.5), ncol=2).draw_frame(False)
 plt.savefig(input[: input.rfind('.')] +'.pdf', bbox_inches='tight', format='pdf')
 
